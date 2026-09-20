fa89d5c40a734c03459f4dabd120770d92cc7d55
Igor Marta <mr.igor.marta@gmail.com>
2026-09-20 18:39:18 +0300

    Add cterm indexes

--- flake.nix
+++ flake.nix
@@ -7,14 +7,38 @@
     _:
     let
       baseColors = {
-        base03 = "#002b36";
-        base02 = "#073642";
-        base01 = "#586e75";
-        base00 = "#657b83";
-        base0 = "#839496";
-        base1 = "#93a1a1";
-        base2 = "#eee8d5";
-        base3 = "#fdf6e3";
+        base03 = {
+          val = "#002b36";
+          idx = 8;
+        };
+        base02 = {
+          val = "#073642";
+          idx = 0;
+        };
+        base01 = {
+          val = "#586e75";
+          idx = 10;
+        };
+        base00 = {
+          val = "#657b83";
+          idx = 11;
+        };
+        base0 = {
+          val = "#839496";
+          idx = 12;
+        };
+        base1 = {
+          val = "#93a1a1";
+          idx = 14;
+        };
+        base2 = {
+          val = "#eee8d5";
+          idx = 7;
+        };
+        base3 = {
+          val = "#fdf6e3";
+          idx = 15;
+        };
       };
       accentColors = {
         yellow = "#b58900";
@@ -26,31 +50,67 @@
         cyan = "#2aa198";
         green = "#859900";
       };
+      ctermAccentColors = {
+        yellow = 3;
+        orange = 9;
+        red = 1;
+        magenta = 5;
+        violet = 13;
+        blue = 4;
+        cyan = 6;
+        green = 2;
+      };
     in
     {
       solarized = with baseColors; {
         light = {
-          background = base3;
-          backHighlight = base2;
-          secondaryContent = base1;
-          primaryContent = base00;
-          highlight = base01;
-          black = base02;
-          brightBlack = base03;
-          brightBlue = base0;
-        }
-        // accentColors;
+          cterm = {
+            background = base3.idx;
+            backHighlight = base2.idx;
+            secondaryContent = base1.idx;
+            primaryContent = base00.idx;
+            highlight = base01.idx;
+            black = base02.idx;
+            brightBlack = base03.idx;
+            brightBlue = base0.idx;
+          }
+          // ctermAccentColors;
+          gui = {
+            background = base3.val;
+            backHighlight = base2.val;
+            secondaryContent = base1.val;
+            primaryContent = base00.val;
+            highlight = base01.val;
+            black = base02.val;
+            brightBlack = base03.val;
+            brightBlue = base0.val;
+          }
+          // accentColors;
+        };
         dark = {
-          background = base03;
-          backHighlight = base02;
-          secondaryContent = base01;
-          primaryContent = base0;
-          highlight = base1;
-          white = base2;
-          brightWhite = base3;
-          brightYellow = base00;
-        }
-        // accentColors;
+          cterm = {
+            background = base03.idx;
+            backHighlight = base02.idx;
+            secondaryContent = base01.idx;
+            primaryContent = base0.idx;
+            highlight = base1.idx;
+            white = base2.idx;
+            brightWhite = base3.idx;
+            brightYellow = base00.idx;
+          }
+          // ctermAccentColors;
+          gui = {
+            background = base03.val;
+            backHighlight = base02.val;
+            secondaryContent = base01.val;
+            primaryContent = base0.val;
+            highlight = base1.val;
+            white = base2.val;
+            brightWhite = base3.val;
+            brightYellow = base00.val;
+          }
+          // accentColors;
+        };
       };
     };
 }
